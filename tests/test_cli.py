5f46b81a41d5959bb105707acc81c24acacd57a1
Sahin Kureta <skureta@gmail.com>
2018-02-25 04:28:20 +0300

    added bjorklund cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,14 +6,15 @@
 from click.testing import CliRunner
 
 from pytom import cli
+from pytom.libs.bjorklund import bjorklund
 
 
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli.main, ['--bjork', 13, 5])
     assert result.exit_code == 0
-    assert 'pytom.cli.main' in result.output
+    assert repr(bjorklund(13, 5)) + '\n' == result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert '--bjork <int> <int>  Euclidean rhthym given a number of steps and a number' in help_result.output
